7a1af44d042155d1f6361724dbf7530a766c4b63
Nathan Lambertson <lambertones@gmail.com>
2023-09-19 16:03:14 -0500

    add index position stuffs

--- arrays.rb
+++ arrays.rb
@@ -11,3 +11,14 @@ puts players.push(6, 7, 8, 9)
 puts work_hrs.pop
 # .unshift will add item to beginning of array
 puts booleans.unshift(!true)
+
+=begin
+Index position is simply the exact item of the array 
+that we wish to select. The positions start with [0] 
+instead of [1], which is a little bit counterintuitive.
+=end
+
+#example of index position:
+print pizza[0] #would print "veggie"
+print pizza[0][3] #prints "g" because it is the 3rd letter in the first element
+print pizza.values_at(0, 3) #prints "veggie", "hawaiian"
